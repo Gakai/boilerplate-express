c2138b13f51c377df64c93804d3d20a76fa90f78
Steffen Kutscher <git@skutscher.de>
2023-02-23 01:05:49 +0100

    Step 3: Serve an HTML File
    Signed-off-by: Steffen Kutscher <git@skutscher.de>

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
+let { join } = require('node:path')
 let express = require('express')
 let app = express()
 
 console.log('Hello World')
 
-app.get('/', (req, res) => res.send('Hello Express'))
+app.get('/', (req, res) => {
+	const indexFile = join(__dirname, 'views', 'index.html')
+	res.sendFile(indexFile)
+})
 
 module.exports = app
